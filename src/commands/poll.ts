bb18dbc07cc4e0b7e758eea1e6b4c8728ceb3982
NeroKatakana <39287552+NeroKatakana@users.noreply.github.com>
2019-08-02 16:20:56 +1000

    first commit

---
+++ src/commands/poll.ts
@@ -0,0 +1,44 @@
+import * as Discord from "discord.js";
+import {IBotCommand} from "../api";
+
+export default class poll implements IBotCommand {
+    
+    private readonly _command = "poll"
+    
+    help(): string {
+        return "Create a basic poll";
+    }
+    
+    isThisCommand(command: string): boolean {
+        return command === this._command;
+    }
+
+    async runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): Promise<void> {
+        
+        //Let me know it worked
+        msgObject.delete(0);
+
+        if(args.length < 1) { return; }
+
+        let pollEmbed = new Discord.RichEmbed()
+            .setTitle("Poll")
+            .setDescription(args.join(" "))
+
+        let pollMessage = await msgObject.channel.send(pollEmbed);
+
+        await (pollMessage as Discord.Message).react("✅");
+        await (pollMessage as Discord.Message).react("❎");
+
+        const filter = (reaction: Discord.MessageReaction) => reaction.emoji.name === "✅" || reaction.emoji.name === "❎";
+        const results = await (pollMessage as Discord.Message).awaitReactions(filter, { time: 10000})
+
+        let resultsEmbed = new Discord.RichEmbed()
+            .setTitle("Poll Results")
+            .setDescription(`Results for The Poll: ${args.join("")}`)
+            .addField("✅:",`${results.get("✅").count-1} Votes`)
+            .addField("❎:",`${results.get("❎").count-1} Votes`)
+
+        msgObject.channel.send(resultsEmbed);
+        (pollMessage as Discord.Message).delete(0);
+    }
+}
--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ export let config = {
         "kick",
         "purge",
         "serverinfo",
+        "poll",
 
     ]
 }    
